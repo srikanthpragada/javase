0b5190e1370d8d415c8021701618367dd81f8301
Srikanth Pragada <srikanthpragada@gmail.com>
2016-06-26 16:10:05 +0530

    Displays Java Source Code for selected file.
    Place icons changedir.png and exit.png (provided separately in this repository) in the same folder as this .java file. 
    
    It uses JScrollPane, JTextArea, JButton, JToolBar,  JFileChooser, JOptionPane, JList, Paths, Path, Files and File classes.
    
    Uses Streams and Lambda expressions.

--- JavaCodeViewer.java
+++ JavaCodeViewer.java
@@ -1,70 +1,132 @@
+package demo;
+
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.Dimension;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 import javax.swing.DefaultListModel;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JList;
 import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JToolBar;
 
 public class JavaCodeViewer extends JFrame {
-	private String dir = "c:\\jdk8\\may6";
-	private JList lstFiles;
+	private String dir = "/Users/srikanthpragada/Documents/workspace/demo/src/demo";
+	private JList<String> lstFiles;
 	private JTextArea taContents;
-	private DefaultListModel files;
+	private DefaultListModel<String> files;
 	private JToolBar toolBar;
-	private JButton  btnChange;
+	private JButton  btnChange, btnExit;
+	private JCheckBox cbLineNumbers;
+	private String title = "Java Code Viewer";
 
 	public JavaCodeViewer() {
-		super("Java Code Viewer");
-		buildUI();
+ 		buildUI();
+		this.setTitle(title + " - " + dir);
 	}
 
 	public void buildUI() {
 
 		taContents = new JTextArea();
-		files = new DefaultListModel();
+		files = new DefaultListModel<String>();
 		loadFiles();
-		lstFiles = new JList(files);
+		lstFiles = new JList<String>(files); 
+		lstFiles.setSize( new Dimension(150,0));
 		lstFiles.setBackground(Color.lightGray);
 		lstFiles.addListSelectionListener(e -> showFile());
 
 		toolBar = new JToolBar();
-        btnChange = new JButton("Change Directory");
+        btnChange = new JButton( getImage("changedir.png"));
+        btnChange.setToolTipText("Change Directory");
         btnChange.addActionListener( e -> changeDir());
         toolBar.add(btnChange);
         
         
+        btnExit = new JButton( getImage("exit.png"));
+        
+        btnExit.setToolTipText("Exit Application");
+        btnExit.addActionListener( e -> System.exit(0));
+        toolBar.add(btnExit);
+        
+        cbLineNumbers = new JCheckBox("Line Numbers");
+        cbLineNumbers.addItemListener(  e -> showFile());
+        toolBar.add(cbLineNumbers);
+        
+        
+        toolBar.setPreferredSize( new Dimension(0,40));
+        
+        
+        
 		Container c = getContentPane();
-		c.add(lstFiles, BorderLayout.LINE_START);
-		c.add(taContents);
+		JScrollPane spFiles = new JScrollPane(lstFiles, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		spFiles.setPreferredSize(new Dimension(150,0));
+		c.add(spFiles, BorderLayout.LINE_START);
+		
+		JScrollPane sp = new JScrollPane(taContents, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		c.add(sp);
 		c.add(toolBar, BorderLayout.PAGE_START);
 	}
 	
 	public void changeDir() {
-	   String input = JOptionPane.showInputDialog(this,"Enter Directory Name", "Directory", JOptionPane.PLAIN_MESSAGE);
-	   if ( input != null)
-	   {
-		   dir = input;
+	   JFileChooser chooser = new JFileChooser(); 
+	    chooser.setCurrentDirectory(new java.io.File("."));
+	    chooser.setDialogTitle("Select Folder");
+	    chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+	    int result = chooser.showOpenDialog(this);
+	    if ( result == JFileChooser.APPROVE_OPTION)
+	    {
+	       
+		   dir = chooser.getSelectedFile().getAbsolutePath();
+		   System.out.println(dir);
 		   loadFiles();
+		   setTitle(title + "  - " + dir);
 		   taContents.setText("");
 	   }
 	}
 
+	
+	public ImageIcon getImage(String filename) {
+		try {
+			return new ImageIcon(this.getClass().getResource(filename));
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+			return null;
+		}
+	}
+	
 	public void showFile() {
 		if ( lstFiles.getSelectedIndex() < 0 )
 			return; 
+		
 		taContents.setText("");
+		
 		try {
-			Path p = Paths.get(dir + "\\" + lstFiles.getSelectedValue());
-			Files.lines(p).forEach( line -> taContents.append(line + "\n"));
+			Path p = Paths.get(dir + File.separator + lstFiles.getSelectedValue());
+		    if ( cbLineNumbers.isSelected()) {
+		    	 int lineno=1;
+			     Stream<String> lines = Files.lines(p);
+			     
+			     for(Object line : lines.toArray())		     {
+			    	 taContents.append(String.format("%4d : %s\n", lineno,line));
+			    	 lineno ++;
+			     }
+		    }
+		    else
+		    	Files.lines(p).forEach( line -> taContents.append(line + "\n"));
+		    
 		} catch (Exception ex) {
             JOptionPane.showMessageDialog(this,ex.getMessage(),"Error", JOptionPane.ERROR_MESSAGE);
 		}
@@ -90,3 +152,4 @@ public class JavaCodeViewer extends JFrame {
 	}
 
 }
+ 
