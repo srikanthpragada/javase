8e6a87aba58789e28bd4175c6902f64c3b32dfd9
Srikanth Pragada <srikanthpragada@gmail.com>
2018-03-07 07:57:00 +0530

    Guessing Number  Game
    Assume a number and allow user to guess in 3 attempts.

---
+++ GuessNumber.java
@@ -0,0 +1,38 @@
+// Guessing number  Program 
+import java.util.Scanner;
+
+public class GuessNumber {
+
+	public static void main(String[] args) {
+
+		// take a random number in the range 0 to 25
+		long number = Math.round(Math.random() * 25);
+
+		int attempts = 1;
+		Scanner s = new Scanner(System.in);
+
+		while (attempts <= 3) {
+
+			System.out.print("Guess Number :");
+			int gn = s.nextInt();
+			
+			if (gn == number) {
+				System.out.println("Congratualtions. You have done it!");
+				return;
+			}
+			// Provide assistance when it is not last attempt 
+			if (attempts < 3) {
+				if (gn > number)
+					System.out.println("Your number is bigger!");
+				else
+					System.out.println("Your number is smaller!");
+			}
+
+			attempts++;
+		}
+
+		
+		System.out.println("Sorry! Next time better luck!");
+	}
+
+}
