6a0d97938f6b6a32138abafdf850348b4cf65239
Srikanth Pragada <srikanthpragada@gmail.com>
2018-12-09 09:06:40 +0530

    Factorial.java
    Takes a number from user and prints factorial

---
+++ Factorial.java
@@ -0,0 +1,17 @@
+// Program to print factorial of the given number
+import java.util.Scanner;
+
+public class Factorial {
+   public static void main(String args[]) {
+   	      Scanner s = new Scanner(System.in);  // read input from keyboard
+
+   	      System.out.print("Enter a number :");
+          int num = s.nextInt();  // read an int 
+ 
+          int fact = 1; 
+          for (int i=2; i <= num; i++)
+          	 fact = fact * i;
+
+          System.out.printf("Factorial of %d is %d", num,fact);
+   }
+}
