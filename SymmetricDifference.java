99a1cd24198835d54e1040e1277d510f182741a7
Srikanth Pragada <srikanthpragada@gmail.com>
2019-11-20 09:20:55 +0530

    Display symmetric difference between two files
    Displays symmetric difference (XOR) between two files

--- SymmetricDifference.java
+++ SymmetricDifference.java
@@ -2,7 +2,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.LinkedHashSet;
 import java.util.List;
-
+ 
 public class SymmetricDifference {
 
 	public static void main(String[] args) throws Exception {
